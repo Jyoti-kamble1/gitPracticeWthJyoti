cf6639c80d4fce745a277b531187e15d998f26ef
kiran2816 <pawarkiran199728@gmail.com>
2026-06-03 13:11:36 +0530

    first commit

---
+++ practice_artifact/src/main/java/com/practice/configuration/BaseClass.java
@@ -0,0 +1,5 @@
+package com.practice.configuration;
+
+public class BaseClass {
+
+}
---
+++ practice_artifact/src/test/java/com/practice/module_name/Demo.java
@@ -0,0 +1,12 @@
+package com.practice.module_name;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void demoValidation() {
+		System.out.println("Demo Class");
+	}
+	
+	
+}
