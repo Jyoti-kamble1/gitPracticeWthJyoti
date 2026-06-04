41fdab04a9de24fa3227caa2b2cb43146725722d
Jyoti-kamble1 <jyoti.kamble9828@gmail.com>
2026-06-04 13:00:48 +0530

    jj

---
+++ practice_artifact/src/test/java/com/practice/module_name/Demo4.java
@@ -0,0 +1,10 @@
+package com.practice.module_name;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("mu repo");
+	}
+
+}
